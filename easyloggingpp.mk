889c683e677e604e20a06b1cc7bb452f366827ed
Yuanhui He <List_View_321@163.com>
2024-12-19 18:16:03 +0800

    New: add Kconfig for easylogingpp

--- easyloggingpp.mk
+++ easyloggingpp.mk
@@ -12,5 +12,8 @@ CCSRCS += $(foreach src, $(M_SRCS), $(SUB_DIR)/$(src))
 
 VPATH += :$(TOP_DIR)/$(SUB_DIR)
 EXT_INCLUDE += -I$(TOP_DIR)/$(SUB_DIR)/src
+EXT_LDFLAGS += -lstdc++
 
-
+ifeq ($(CONFIG_THIRDPART_EASYLOGINGPP_CONFIG_ENABLE), y)
+	EXT_CFLAGS += -DELPP_NO_DEFAULT_LOG_FILE
+endif
